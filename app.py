191598d91e2715ff1dc952de33b0ae080ab8970e
M3MAI <160223496+M3MAI@users.noreply.github.com>
2024-02-16 18:23:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,4 +16,4 @@ def index():
     return render_template('index.html', default_printer=default_printer)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
