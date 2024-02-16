8151da46b4dece32d58d094ee580701f6a197f1d
M3MAI <160223496+M3MAI@users.noreply.github.com>
2024-02-16 18:57:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
-from flask import Flask, render_template
-import win32print
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+from flask import Flask
 
+# Flask constructor takes the name of 
+# current module (__name__) as argument.
 app = Flask(__name__)
 
-def get_default_printer():
-    try:
-        default_printer = win32print.GetDefaultPrinter()
-        return default_printer
-    except Exception as e:
-        return str(e)
-
+# The route() function of the Flask class is a decorator, 
+# which tells the application which URL should call 
+# the associated function.
 @app.route('/')
-def index():
-    default_printer = get_default_printer()
-    return render_template('index.html', default_printer=default_printer)
+# ‘/’ URL is bound with hello_world() function.
+def hello_world():
+	return 'Hello World'
 
+# main driver function
 if __name__ == '__main__':
-    app.run()
+
+	# run() method of Flask class runs the application 
+	# on the local development server.
+	app.run()
